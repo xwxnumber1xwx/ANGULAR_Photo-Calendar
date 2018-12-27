53ca8cb101d6fcaff867a4c3270d539dd60147f8
Daniele Caputo <caputo.daniele.n1@gmail.com>
2018-12-27 22:00:37 +0100

    add calendar functions

--- src/app/main-calendar/main-calendar.component.ts
+++ src/app/main-calendar/main-calendar.component.ts
@@ -11,24 +11,61 @@ export class MainCalendarComponent implements OnInit {
   date: Date;
   year: number;
   month: any[] = [];
+  monthName: string = '';
 
-  constructor( ) { }
+  constructor() { }
 
   ngOnInit() {
     this.date = new Date;
+    this.month = this.createCalendarDate();
+  }
+
+  createCalendarDate(): any {
+    let month: any[] = [];
     this.year = this.date.getFullYear();
-    let m = this.date.getMonth();
-    let daysOfMonth = 31;
-    if ((m === 3) || m === 5 || m===8 || m ===10) {
-      daysOfMonth = 30;
-    } else if (m===1) {
-      daysOfMonth = 28;
+    this.monthName = this.getMonthName(this.date.getMonth());
+    let daysOfMonth = this.getFullMonth(this.date.getMonth());
+    let daysOfLastMonth = this.getFullMonth(this.date.getMonth() - 1);
+    this.date.setDate(1);
+    let firstDay = this.date.getDay();
+    console.log("firstDay " + firstDay);
+    let m: number[] = [];
+    // +1 mean week start sunday, TODO: +2 for monday but got some error to fix
+    for (let i = -firstDay+1; i <= daysOfMonth; i++) {
+      if (i<1) {
+        month.push(daysOfLastMonth - i - firstDay+1)
+      } else
+      m.push(i);
     }
-    let day = this.date.getDay();
-    for(let i = -day; i <= daysOfMonth; i++) {
-        i < 1 ? this.month.push('') : this.month.push(i);
+    month.reverse();
+    month = month.concat(m);
+    return month;
+  }
+
+  nextMonth(): void {
+    this.date.setMonth(this.date.getMonth()+1);
+    this.month = this.createCalendarDate();
+  }
+  lastMonth(): void {
+    this.date.setMonth(this.date.getMonth()-1);
+    this.month = this.createCalendarDate();
+  }
+
+  getFullMonth(m): number {
+    if (m < 0) {
+      m = 11;
     }
-    console.log(this.month);
+    if ((m === 3) || m === 5 || m === 8 || m === 10) {
+      return 30;
+    } else if (m === 1) {
+      return 28;
+    }
+    return 31;
+  }
+
+  getMonthName(n): string {
+    let months    = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    return months[n];
   }
 
   UseThisPicture(url: any) {
